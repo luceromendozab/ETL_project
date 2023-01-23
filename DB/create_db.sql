f85d6e00c5eb5752c7d33d1d3505c8a7e5b063e1
luceromendozab <luceromendoz@gmail.com>
2023-01-23 17:53:26 +0100

    last

--- DB/create_db.sql
+++ DB/create_db.sql
@@ -5,87 +5,62 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema endangerment_languages
+-- Schema endlang_usa
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema endangerment_languages
+-- Schema endlang_usa
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `endangerment_languages` DEFAULT CHARACTER SET utf8 ;
-USE `endangerment_languages` ;
+CREATE SCHEMA IF NOT EXISTS `endlang_usa` DEFAULT CHARACTER SET utf8 ;
+USE `endlang_usa` ;
 
 -- -----------------------------------------------------
--- Table `endangerment_languages`.`languages`
+-- Table `endlang_usa`.`languages_usa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `endangerment_languages`.`languages` (
-  `name_language` VARCHAR(45) NOT NULL,
-  `cuntries` VARCHAR(45) NOT NULL,
-  `endangerment` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `endlang_usa`.`languages_usa` (
+  `id_lang_usa` INT NOT NULL,
+  `language_name` VARCHAR(200) NOT NULL,
+  `endangerment` VARCHAR(200) NOT NULL,
   `speakers` INT NOT NULL,
   `latitude` FLOAT NOT NULL,
   `longitude` FLOAT NOT NULL,
-  PRIMARY KEY (`name_language`))
+  `state` VARCHAR(200) NOT NULL,
+  PRIMARY KEY (`id_lang_usa`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `endangerment_languages`.`languages_usa`
+-- Table `endlang_usa`.`states_population`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `endangerment_languages`.`languages_usa` (
-  `language_name` VARCHAR(45) NOT NULL,
-  `endangerment` VARCHAR(45) NOT NULL,
-  `speakers` INT NOT NULL,
-  `state_name` VARCHAR(45) NOT NULL,
-  `languages_name_language` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`language_name`),
-  INDEX `fk_languages_usa_languages_idx` (`languages_name_language` ASC) VISIBLE,
-  CONSTRAINT `fk_languages_usa_languages`
-    FOREIGN KEY (`languages_name_language`)
-    REFERENCES `endangerment_languages`.`languages` (`name_language`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `endangerment_languages`.`population_usa`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `endangerment_languages`.`population_usa` (
-  `state_name` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `endlang_usa`.`states_population` (
+  `state_name` VARCHAR(200) NOT NULL,
   `1970` INT NOT NULL,
   `1980` INT NOT NULL,
   `1990` INT NOT NULL,
   `2000` INT NOT NULL,
   `2010` INT NOT NULL,
   `2020` INT NOT NULL,
-  `languages_usa_language_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`state_name`),
-  INDEX `fk_population_usa_languages_usa1_idx` (`languages_usa_language_name` ASC) VISIBLE,
-  CONSTRAINT `fk_population_usa_languages_usa1`
-    FOREIGN KEY (`languages_usa_language_name`)
-    REFERENCES `endangerment_languages`.`languages_usa` (`language_name`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`state_name`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `endangerment_languages`.`ethnic_population`
+-- Table `endlang_usa`.`states_population_has_languages_usa`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `endangerment_languages`.`ethnic_population` (
-  `state_name` VARCHAR(45) NOT NULL,
-  `1970` INT NOT NULL,
-  `1980` INT NOT NULL,
-  `1990` INT NOT NULL,
-  `2000` INT NOT NULL,
-  `2010` INT NOT NULL,
-  `2020` INT NOT NULL,
-  `population_usa_state_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`state_name`),
-  INDEX `fk_ethnic_population_population_usa1_idx` (`population_usa_state_name` ASC) VISIBLE,
-  CONSTRAINT `fk_ethnic_population_population_usa1`
-    FOREIGN KEY (`population_usa_state_name`)
-    REFERENCES `endangerment_languages`.`population_usa` (`state_name`)
+CREATE TABLE IF NOT EXISTS `endlang_usa`.`states_population_has_languages_usa` (
+  `states_population_state_name` VARCHAR(200) NOT NULL,
+  `languages_usa_id_lang_usa` INT NOT NULL,
+  PRIMARY KEY (`states_population_state_name`, `languages_usa_id_lang_usa`),
+  INDEX `fk_states_population_has_languages_usa_languages_usa1_idx` (`languages_usa_id_lang_usa` ASC) VISIBLE,
+  INDEX `fk_states_population_has_languages_usa_states_population_idx` (`states_population_state_name` ASC) VISIBLE,
+  CONSTRAINT `fk_states_population_has_languages_usa_states_population`
+    FOREIGN KEY (`states_population_state_name`)
+    REFERENCES `endlang_usa`.`states_population` (`state_name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_states_population_has_languages_usa_languages_usa1`
+    FOREIGN KEY (`languages_usa_id_lang_usa`)
+    REFERENCES `endlang_usa`.`languages_usa` (`id_lang_usa`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -94,4 +69,6 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
 
--- README.md
+++ README.md
@@ -5,11 +5,22 @@
 
 ¿Son las lenguas en peligro de extinción importantes?. ¿Sabemos aproximadamente cuántas existen o cuales son?
 
-Estas preguntas me surgieron antes de realizar este proyecto ; todo empezó con un csv que 
+Estas preguntas me surgieron antes de realizar este primer proyecto hacia mi camino a convrtirme en una  analista de datos. 
+
+Analizando y limpiando  el primer csv que obtuve , descubrí que el país que más lenguas en peligro de extinción tenia era Estados Unidos asi que me decanté por analizar las lenguas en peligro de extinción por estados y ver la relación que tuvieran con la población de cada estado a lo largo de los últimos 50 años.
+
+Las lenguas son las que definen nuestro como de ver la vida, 
+## Data
+
+Los datos obtenidos son a travez de la descarga de un csv de Kaggle y de el webscraping 
+
+kaggle: https://www.kaggle.com/datasets/the-guardian/extinct-languages 
+
+Webscraping:https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_historical_population"
 
 ## Libraries 
 
-- Pandas:pandas library for data manipulation and analysis;it offers data structures and operations for manipulating numerical tables and time series. 
+- <br> Pandas </br> :pandas library for data manipulation and analysis;it offers data structures and operations for manipulating numerical tables and time series. 
 https://pandas.pydata.org/ 
 
 - Requests: allows you to send HTTP requests using Python.
@@ -23,3 +34,4 @@ https://pypi.org/project/requests/
 - Marplotlib:
 - Seaborn:
 - Folium: 
+- sql alquemy
